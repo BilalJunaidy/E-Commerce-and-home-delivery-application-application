df2ea969feaa4878df6e1828dd4cec68686c149e
Bilal Junaidy <bilal.junaidy@gmail.com>
2021-01-08 20:56:10 +0500

    Comments added about get_or_create

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -17,7 +17,24 @@ def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer 
 
-        
+        # I like this approach since initially (at least based on how the app is currently structured) there will be no Order object for each customer. 
+        # Therefore, what the below get_or_create function will let us do is that when the user first tries to go to the /cart route, a new Order object 
+        # will be created. 
+        # Upon subsequent visits, the user will basically be "getting" this Order object (as it has already been created already) and therefore, this 
+        # function call works. 
+        # One caveat though.....if we have created this object, then we need to save it as well. 
+        # Therefore, if the variable "created" below returns a True, then we need to save the Order object that has just been created by adding the following 
+        # line of code:
+
+        # if created = True:
+        #     order.save()
+
+        # The alternative method here would be to simply "get" the correct order based on the value passed in as parameters to the get_or_create function below.
+        # So how do we deal with the creation and the subsequent saving of the Order object?
+        # Well, we can basically use signals, similar to how you used them to automatically created objects of the Profile model class in the other Django app you were building last week. 
+        # This way, whenever a user account is created and saved (hint: post_save signal), it would send a signal to the Order model class which would result 
+        # in the creation and saving of an Order object. 
+
         order, created = Order.objects.get_or_create(customer = customer, completed = False)
         items = order.orderitem.all()
         
